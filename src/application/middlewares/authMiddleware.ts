1d91bf98c2bcd35ed659ec4de39a098d6b849405
robson-carvalho <robsoncarvalho.tech@gmail.com>
2025-01-29 14:05:49 -0300

    feat: adiciona middleware de autenticação

---
+++ src/application/middlewares/authMiddleware.ts
@@ -0,0 +1,37 @@
+import { Request, Response, NextFunction } from "express";
+
+import { ForbiddenError } from "../../core/errors/Errors";
+import { JWT } from "../../infrastructure/utils/JWT";
+
+type TokenPaylod = {
+  id: string;
+  iat: number;
+  exp: number;
+};
+
+const AuthMiddleware = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  const { authorization } = req.headers;
+
+  if (!authorization) {
+    return next(new ForbiddenError("Token not provided"));
+  }
+
+  const [, token] = authorization.split(" ");
+
+  try {
+    const decoded = await JWT.getInstance().verify(token);
+    const { id } = decoded as TokenPaylod;
+
+    req.user_id = id;
+
+    next();
+  } catch (error) {
+    return next(new ForbiddenError("Token invalid"));
+  }
+};
+
+export { AuthMiddleware };
