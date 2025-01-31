95c456f5389512135b24a5528b31bb9aaf6cec54
robson-carvalho <robsoncarvalho.tech@gmail.com>
2025-01-31 00:36:56 -0300

    libera qualquer url realizar request

--- src/app.ts
+++ src/app.ts
@@ -9,7 +9,11 @@ const app: Application = express();
 
 MongoDB.getInstance().connect();
 
-app.use(cors());
+app.use(
+  cors({
+    origin: "*",
+  })
+);
 app.use(express.json());
 app.use("/api/v1", router);
 app.use(errorHandler);
